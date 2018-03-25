e92adcc176e4e4e9cb15ba11e665319ba0bef9d0
Jared Wines <jwines760@gmail.com>
2018-03-24 23:09:37 -0700

    add read for txt function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
 function myFunction() {
+	//var text2 = getLinesFromFront(/fronts/front1.txt);
+	var text2 = getLinesFromFront('C:/Users/surfe/GitHub/text-to-ascii-art/fronts/front1.txt');
     var text = document.getElementById("theText").value;
-    document.getElementById("ascii-art-text").innerHTML = text;
+    document.getElementById("ascii-art-text").innerHTML = text2;
 }
 
-((assicCharDec - 32) * 8) + 2
+function getLinesFromFront(filepath) {
+	var str = "";
+	var lines = "";
+	var txtFile = new File(filepath);
+	txtFile.open("r");
+	while (!txtFile.eof) {
+		// read each line of text
+		str += txtFile.readln() + "\n";
+	}
+	//lines = str.split("\n");
+	return str;
+}
+
+//((assicCharDec - 32) * 8) + 2
