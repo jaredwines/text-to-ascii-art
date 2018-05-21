b3ba9d49dcf086f33669d49e5ca9183a6244a9bd
Jared Wines <jwines760@gmail.com>
2018-05-21 14:46:09 -0700

    Update Commits

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,5 @@
 var input = document.getElementById("users-input");
+
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -6,11 +7,13 @@ input.addEventListener("keyup", function(event) {
     }
 })
 
+//Creats Assic art from the user's input and displays the result in text area
 function genAssicArt() {
 	var assicArt = "";
 	var userText = document.getElementById("users-input").value;
 	
 	var row = 0;
+	//while loop is set to less than 7 because there are 7 rows of assic art for each character
 	while (row < 7)
 	{
 		for (var i = 0; i < userText.length; i++) 
@@ -23,6 +26,7 @@ function genAssicArt() {
 	document.getElementById("ascii-art-text").innerHTML = assicArt;
 }
 
+//gets the correct line of the desired character.
 function getPieceOfArt(text, textPos, rowPos)
 {
 	var frontArrPos = ((text.charCodeAt(textPos) - 32) * 7) + rowPos;
@@ -31,21 +35,16 @@ function getPieceOfArt(text, textPos, rowPos)
 	return pieceOfArt;
 }
 
+//copies the assic art text area to pc's clipboard
 function CopyToClipboard(containerid) {
-  // Create a new textarea element and give it id='temp_element'
   var textarea = document.createElement('textarea')
   textarea.id = 'temp_element'
-  // Optional step to make less noise on the page, if any!
   textarea.style.height = 0
-  // Now append it to your page somewhere, I chose <body>
   document.body.appendChild(textarea)
-  // Give our textarea a value of whatever inside the div of id=containerid
   textarea.value = document.getElementById(containerid).innerText
-  // Now copy whatever inside the textarea to clipboard
   var selector = document.querySelector('#temp_element')
   selector.select()
   document.execCommand('copy')
-  // Remove the textarea
   document.body.removeChild(textarea)
   
   //Display copy notification
